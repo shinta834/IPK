5938fd2b956256251e0516835e8c7edae67f8d1e
shinta834 <80763062+shinta834@users.noreply.github.com>
2021-03-18 00:05:21 +0700

    Update IPK.py

--- IPK.py
+++ IPK.py
@@ -1,6 +1,6 @@
 class students (object):
     def __init__(self):
-        self.nim = 0
+        self.nim = ''
         self.nama = ''
         self.alamat = ''
         self.asalSekolah = ''
@@ -13,7 +13,8 @@ class students (object):
         self.ipkAkhir = 0
         self.beasiswa = ''
     def enterData (self):
-        self.nim = int (input('Nim: '))
+        # input data mulai dari NIM s.d. Nilai TM
+        self.nim = input('Nim: ')
         self.nama = input('Nama: ')
         self.alamat = input('Alamat: ')
         self.asalSekolah = input('Asal Sekolah: ')
@@ -22,18 +23,25 @@ class students (object):
         self.uts = float(input('Nilai UTS: '))
         self.uas = float(input('Nilai UAS: '))
         self.tm = float(input('Nilai TM: '))    
+        # perhitungan nilai IPS
         self.ips = (0.3*self.uts)+(0.3*self.tm)+(0.4*self.uas)
+        # perhitungan nilai IPK akhir
         self.ipkAkhir = (self.ipkAwal + self.ips)/2
+        # penentuan beasiswa yang didapatkan
         if self.kodeProdi == 'TI' or self.kodeProdi == 'SI':
             if self.ipkAkhir >= 75 and self.ipkAkhir < 85:
                 self.beasiswa = '20%'
-            else:
+            elif self.ipkAkhir >= 85 and self.ipkAkhir <=100:
                 self.beasiswa = '30%'
+            else:
+                self.beasiswa = '-'
         else:
             if self.ipkAkhir >= 75 and self.ipkAkhir < 85:
                 self.beasiswa = '25%'
+            elif self.ipkAkhir >= 85 and self.ipkAkhir <=100:
+                self.beasiswa = '35%'
             else:
-                self.beasiswa = '35%' 
+                self.beasiswa = '-'  
     def printData(self):
         print('-----------------------')
         print('Nim: ', self.nim)
@@ -50,13 +58,15 @@ class students (object):
         print('Beasiswa: ',self.beasiswa)
         print('-----------------------')
 
-
+# buat list kosong untuk menyimpan data murid
 list1 = []
+# buat user input untuk menentukan jumlah data murid yang akan dimasukkan
 x = int(input('Masukkan jumlah murid: '))
+# menyimpan data murid yang telah dimasukkan ke dalam list kosong
 for i in range (x):
     a = students()
     a.enterData()
     list1.append(a)
-
+# menampilkan data murid secara lengkap serta data beasiswa
 for i in range (x):
     list1[i].printData()
